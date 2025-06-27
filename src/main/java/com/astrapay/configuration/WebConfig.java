e4b88570a22c45b23f577552cba12227519945d7
fundayluck <afandayul@gmail.com>
2025-06-28 00:09:41 +0700

    adding dynamic port cors, and edit readme

--- src/main/java/com/astrapay/configuration/WebConfig.java
+++ src/main/java/com/astrapay/configuration/WebConfig.java
@@ -1,5 +1,7 @@
 package com.astrapay.configuration;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
